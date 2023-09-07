72b2bcc3b44e025dc6d58dad90a36e4d5cb1ae65
vincent <vincenttwwc@gmail.com>
2023-09-07 22:28:52 +0800

    second
    Signed-off-by: vincent <vincenttwwc@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"ginchat/router"
+)
+
 func main() {
 
+	r := router.Router()
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- test/testGorm.go
+++ test/testGorm.go
@@ -1,6 +1,8 @@
-package test
+package main
 
 import (
+	"fmt"
+	"ginchat/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,28 +14,31 @@ type Product struct {
 }
 
 func main() {
-	db, err := gorm.Open(mysql.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:33306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// 迁移 schema
-	db.AutoMigrate(&Product{})
+	// 迁移 schema 没有表就帮忙创建表
+	db.AutoMigrate(&models.UserBasic{})
+
+	user := &models.UserBasic{}
+	user.Name = "vincent"
 
 	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
+	db.Create(user)
 
 	// Read
-	var product Product
-	db.First(&product, 1)                 // 根据整型主键查找
-	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	//var product Product
+	fmt.Println(db.First(user, 1)) // 根据整型主键查找
+	//db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
 
 	// Update - 将 product 的 price 更新为 200
-	db.Model(&product).Update("Price", 200)
+	db.Model(user).Update("Password", "123456")
 	// Update - 更新多个字段
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	//db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	//db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
 	// Delete - 删除 product
-	db.Delete(&product, 1)
+	//db.Delete(&product, 1)
 }
